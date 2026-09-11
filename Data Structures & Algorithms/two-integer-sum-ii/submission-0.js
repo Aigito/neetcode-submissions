fa73b324c1273c4b82427515bfe4faf32d50bc04
Dylan Pee <peedylan94@gmail.com>
2026-09-11 18:35:01 +0930

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let i = 0;
+        let j = numbers.length - 1
+
+        while (i < j) {
+            if (numbers[i] + numbers[j] > target) {
+                j--;
+                continue
+            }
+
+            if (numbers[i] + numbers[j] < target) {
+                i++;
+                continue;
+            }
+
+            if (numbers[i] + numbers[j] == target) return [i + 1, j + 1]
+
+            return "No answer"
+        }
+    }
+}
